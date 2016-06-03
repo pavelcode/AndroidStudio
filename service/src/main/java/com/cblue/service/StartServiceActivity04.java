027143c4dc0eed3426d1eb92b74c2724c9d0134b
pavelcode <pavel686@163.com>
2016-06-03 09:25:05 +0800

    无聊的更新

---
+++ service/src/main/java/com/cblue/service/StartServiceActivity04.java
@@ -0,0 +1,16 @@
+package com.cblue.service;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 传智：使用启动Service，做一个电话窃听器
+ */
+public class StartServiceActivity04 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startservice_activity04);
+    }
+}
