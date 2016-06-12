1e9152a7d6e34211d0ce3871fc27f74be7e7bd44
pavelcode <pavel686@163.com>
2016-06-12 22:26:59 +0800

    电池广播进行修改

--- broadcastreceiver/src/main/java/com/cblue/broadcastreceiver/BroadcastReceiver_Battery.java
+++ broadcastreceiver/src/main/java/com/cblue/broadcastreceiver/BroadcastReceiver_Battery.java
@@ -3,6 +3,8 @@ package com.cblue.broadcastreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.util.Log;
 
 /**
@@ -19,17 +21,31 @@ public class BroadcastReceiver_Battery extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+
+        Intent batteryIntent = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        //获得剩余当前电量
+        int level1 = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        //电量最大值
+        int scale1 = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        Log.i("aaa","level="+level1);
+        Log.i("aaa","scale="+scale1);
+
+        Log.i("aaa","总电量为："+((level1*100)/scale1)+"%");
+
+
         if(intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)){
-            //获得当前电量
-            int level = intent.getIntExtra("level", 0);
-            //总电量刻度
-            int scale = intent.getIntExtra("scale", 100);
+            //获得剩余当前电量
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            //电量最大值
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+            Log.i("aaa","level="+level);
+            Log.i("aaa","scale="+scale);
 
             Log.i("aaa","总电量为："+((level*100)/scale)+"%");
 
-            if(level<15){
-                //做什么事情
-            }
+
         }
     }
 }
