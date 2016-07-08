bb3593de6fc6548127ce21b24e71a15158285098
pavelcode <pavel686@163.com>
2016-07-08 17:49:35 +0800

    国际化

---
+++ userinterface/src/main/java/com/cblue/adapter/language/LanguageAdapterActivity.java
@@ -0,0 +1,15 @@
+package com.cblue.adapter.language;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cblue.androidstudio.R;
+
+public class LanguageAdapterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language_adapter);
+    }
+}
